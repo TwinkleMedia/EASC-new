3c145a06bf0eeff51b9642e18da2cfc2ef2c4694
TwinkleMedia <hs0052264@gmail.com>
2025-04-19 11:22:18 +0530

    signup done

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './login/Login';
 import Dashboard from './dashboard/dashboard';
+import UploadCourses from './dashboard/page/UploadCourses';
+import CouponForm from './dashboard/page/CouponForm';
+import DashboardOverview from './dashboard/DashboardOverview';
+
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login/>} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />}>
+          {/* Nested routes that will render within the Dashboard component */}
+          <Route index element={<DashboardOverview />} />
+          <Route path="upload-courses" element={<UploadCourses />} />
+          <Route path="create-coupon-code" element={<CouponForm />} />
+          <Route path="*" element={<Navigate to="/dashboard" replace />} />
+        </Route>
+        {/* Redirect any other routes to dashboard */}
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </Router>
   );
